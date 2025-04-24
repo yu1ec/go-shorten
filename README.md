cd198292eb0f335321c31f2e9ba78a598db6f225
yu1ec <i5mwjtci@duck.com>
2025-04-24 18:39:55 +0800

    feat: 新增auth basic授权校验

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 最快实现短链接跳转
 
+## 容器
+`docker pull ghcr.io/yu1ec/go-shorten:latest`
+
 ### Running the Application
 
 本地运行
@@ -23,6 +26,10 @@ The server will start on `localhost:8080`. You can then use the endpoints to sho
 | short_code  | string | 否       | 自定义短码，不传则自动生成 |
 | remark      | string | 否       | 备注         |
 
+> **注意：**  
+> 该接口需要通过 HTTP Basic Auth 认证。  
+> 你需要在请求头中添加 `Authorization: Basic xxx`，其中 `xxx` 是 `SHORTEN_AUTH_USER:SHORTEN_AUTH_PASS` 的 base64 编码。
+
 **请求示例：**
 ```json
 {
@@ -30,6 +37,14 @@ The server will start on `localhost:8080`. You can then use the endpoints to sho
   "short_code": "abc123",
   "remark": "示例"
 }
+```
+
+**curl 示例：**
+```bash
+curl -X POST http://localhost:5768/shorten \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Basic $(echo -n 'xxx:pass' | base64)" \
+  -d '{"target_url":"https://example.com","short_code":"abc123","remark":"示例"}'
 ```
 
 **响应：**
@@ -52,6 +67,9 @@ services:
     container_name: go-shorten
     ports:
       - "5768:5768"
+    environment:
+      SHORTEN_AUTH_USER: xxx
+      SHORTEN_AUTH_PASS: pass
     volumes:
       - ./data:/app/data
     restart: unless-stopped
--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/yu1ec/go-shorten/internal/handler"
 )
 
+// BasicAuthMiddleware 中间件，使用基本认证
+func basicAuth(next http.HandlerFunc, username, password string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		if !strings.HasPrefix(auth, "Basic ") {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
+			return
+		}
+		payload, _ := base64.StdEncoding.DecodeString(strings.TrimPrefix(auth, "Basic "))
+		pair := strings.SplitN(string(payload), ":", 2)
+		if len(pair) != 2 || pair[0] != username || pair[1] != password {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
+
 func main() {
+	username := os.Getenv("SHORTEN_AUTH_USER")
+	password := os.Getenv("SHORTEN_AUTH_PASS")
+
+	if username == "" || password == "" {
+		slog.Warn("For system security, it is recommended to set environment variables. SHORTEN_AUTH_USER and SHORTEN_AUTH_PASS")
+	}
+
 	// POST 创建短链接记录
-	http.HandleFunc("/shorten", handler.ShortenHandler)
+	http.HandleFunc("/shorten", basicAuth(handler.ShortenHandler, username, password))
 	// GET 获取短链接记录进行跳转
 	http.HandleFunc("/", handler.RedirectHandler)
 
 	log.Println("Starting server on :5768")
 	if err := http.ListenAndServe(":5768", nil); err != nil {
-		log.Fatalf("Could not start server: %s\n", err)
+		slog.Error("Could not start server", slog.Any("error", err))
+		os.Exit(1)
 	}
 }
